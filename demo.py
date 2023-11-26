e711a0cfbb52609b59714b0cf2a1c6a400e2e58f
lovanshu <lovanshugarg22703@gmail.com>
2023-11-26 17:59:23 +0530

    1st file

---
+++ demo.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.title('Hello World')
+
+st.header('This is a header')
+
+st.subheader('This is a subheader')
+
+st.write('This is a normal text')
+
+st.markdown("""# h1 tag
+## h2 tag
+### h3 tag
+:moon:<br>
+:sunglasses:
+**bold text**
+_italic_text_
+""",True)
+
+st.latex(r'''''')
+
+st.write("har","riok","ikf")
